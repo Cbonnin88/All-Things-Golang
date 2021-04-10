ecab8496b3c4452f455e77814cdedf6591599e0e
chris <chrisbit.carrington@gmail.com>
2021-04-10 18:56:38 +0200

    a simple GUI using Go and fyne

--- Golang/Projects/GUIgo/GUItest.go
+++ Golang/Projects/GUIgo/GUItest.go
@@ -9,20 +9,26 @@ import (
 )
 
 func main() {
+	// Creating the Gui Window
 	a := app.New()
-	w := a.NewWindow("Money Exchange")
-	lightTheme := theme.LightTheme()
+	w := a.NewWindow("Go GUI")
+	lightTheme := theme.LightTheme() // changes the color theme from dark to light
 	a.Settings().SetTheme(lightTheme)
 
-	hello := widget.NewLabel("Currency Converter:")
+	// Creating a label for the GUI
+	hello := widget.NewLabel("Watch me change")
+
+	// creating a container for the content within the GUI
 	w.SetContent(container.NewVBox(
 		hello,
-		widget.NewButton("Click here to continue", func() {
-			hello.SetText("Choose a currency to change over:")
+		widget.NewButton("Click here", func() { // Creates a button
+			hello.SetText("This is my first GUI in Go") // Sets a text when you use the button
 		}),
-		widget.NewButton("Quit", func() {
+		widget.NewButton("Quit", func() { // Creates a quit button function
 			a.Quit()
 		})))
+
+	// Resizing the GUI Window
 	w.Resize(fyne.NewSize(500, 400))
-	w.ShowAndRun()
+	w.ShowAndRun() // running the GUI
 }
