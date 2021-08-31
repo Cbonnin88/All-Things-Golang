b9c7cf360564bebcfadd84f71719ba868d7f7085
Chris <chrisbit.carrington@gmail.com>
2021-08-31 12:01:30 +0200

    finished testing tutorial

--- Lessons/S_Testing/calculator_test.go
+++ Lessons/S_Testing/calculator_test.go
@@ -7,3 +7,24 @@ func TestCalculator(t *testing.T) {
 		t.Error("Expected 3 * 2 to equal 6")
 	}
 }
+
+func TestTableCalculate(t *testing.T) {
+	var tests = []struct {
+		input    int
+		expected int
+	}{
+		{3, 6},
+		{6, 12},
+		{9, 18},
+		{2, 4},
+		{0, 0},
+		{1, 2},
+		{10, 20},
+	}
+
+	for _, test := range tests {
+		if output := Math(test.input); output != test.expected {
+			t.Error("Test Failed: {} input, {} expected, received:{}", test.input, test.expected, output)
+		}
+	}
+}
