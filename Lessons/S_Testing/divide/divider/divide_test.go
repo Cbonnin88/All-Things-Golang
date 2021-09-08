f24301c20d5af4830e8f1b6275358ef97a2fddd0
Chris <chrisbit.carrington@gmail.com>
2021-09-08 17:57:43 +0200

    Added testing table and benchmark test to divide_test.go

--- Lessons/S_Testing/divide/divider/divide_test.go
+++ Lessons/S_Testing/divide/divider/divide_test.go
@@ -1,9 +1,44 @@
 package divider
 
-import "fmt"
+import (
+	"fmt"
+	"testing"
+)
+
+func TestDivide(t *testing.T) {
+	divide := Divide(3, 12)
+	if divide != 4 {
+		t.Error("Expected:", 4, "Got:", divide)
+	}
+
+}
+
+func TestTableDivide(t *testing.T) {
+	var tests = []struct {
+		firstInput  int
+		secondInput int
+		answer      int
+	}{
+		{20, 100, 5},
+		{6, 30, 5},
+		{3, 12, 4},
+		{4, 8, 2},
+	}
+	for _, test := range tests {
+		if output := Divide(test.firstInput, test.secondInput); output != test.answer {
+			t.Error("Expected:", test.answer, "Got:", output)
+		}
+	}
+}
 
 func ExampleDivide() {
 	fmt.Println(Divide(25, 100))
 	// Output:
 	// 4
+}
+
+func BenchmarkDivide(b *testing.B) {
+	for d := 0; d < b.N; d++ {
+		Divide(3, 12)
+	}
 }
